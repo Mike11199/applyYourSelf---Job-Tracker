b9302f7c6ac9549d7e8367d7908411979126f759
Michael <miwanek11199@gmail.com>
2022-06-01 18:14:28 -0700

    added styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import Landing from "./pages/Landing"
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: red;
+  color:white;
+  font-size: 1rem;
+`
+const ButtonSecond = styled.button`
+  background: blue;
+  color:white;
+  font-size: 1rem;
+`
 
 function App() {
   return (
     <div>
+      <ButtonSecond>Click ME</ButtonSecond>
+      <Button>Click ME</Button>
       <h1>applyYourSelf</h1>
       <Landing />
     </div>
