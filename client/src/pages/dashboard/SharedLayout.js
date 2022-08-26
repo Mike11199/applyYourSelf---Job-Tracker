d303d310c092e3e9d9d3adf1e02ab5b9c412665f
Michael <miwanek11199@gmail.com>
2022-08-25 22:31:29 -0700

    set up navbar structure and components for sidebars, added react icons package

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,6 +1,25 @@
+import { Outlet, Link } from "react-router-dom"
+import Wrapper from '../../assets/wrappers/SharedLayout'
+import { Navbar, SmallSidebar, BigSidebar } from '../../components'
+
+
+// wrapper comes from shared layout
+// handled by css in wrappers: BigSidebar is shown if screen size large, show SmallSidebar if screen size is mobile
 export const SharedLayout = () => {
     return (
-      <div>shared layout</div>
+    <Wrapper>  
+      <main className="dashboard">
+      
+  <SmallSidebar/>
+  <BigSidebar/>
+      <div>
+        <Navbar/>
+        <div className="dashboard-page">
+           <Outlet />
+        </div>
+      </div>
+      </main>
+    </Wrapper>
     )
   }
   
