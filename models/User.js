954adb315d0eb9eb570f29fc4acf17507724389a
Michael <miwanek11199@gmail.com>
2022-08-29 00:15:11 -0700

    added back end functionality for updating the user profile info

--- models/User.js
+++ models/User.js
@@ -43,6 +43,8 @@ const userSchema = new mongoose.Schema({
 
 // set up middleware for MongoDB for hashing the password
 userSchema.pre('save', async function(){
+
+    if(!this.isModified('password'))return
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
     //console.log(this)
