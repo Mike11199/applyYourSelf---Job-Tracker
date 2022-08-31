a8053552340277a3d85e02b509b266d8a6850d74
Michael <miwanek11199@gmail.com>
2022-08-30 21:39:49 -0700

    set up job schema and job controller in back end

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,8 +1,18 @@
+import Job from '../models/Job.js'
+import { StatusCodes } from 'http-status-codes'
+import { BadRequestError, UnAuthenticatedError } from '../errors/index.js'
 
 
 // async because we are communicating with the server
 const createJob = async (req, res) => {
-    res.send('create job')
+    const {position, company } = req.body
+    
+    if (!position || ! company) {
+        throw new BadRequestError('Please provide all values')
+    }
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 const getAllJobs = async (req, res) => {
